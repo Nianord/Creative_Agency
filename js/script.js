a228e02ada113b7a1899ff195c5bc0582058bea1
Olga <74980099+Nianord@users.noreply.github.com>
2023-07-30 17:45:40 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -38,7 +38,7 @@ let myImageSlider = new Swiper('.slider', {
 	watchOverflow: true,
 	spaceBetween: 20,
 	loop: true,
-	loopedSlides: 2,
+	loopedSlides: 5,
 	effect: 'slide',
 	centeredSlides: true,
 });
